3013afc77725665376d996f9f63535398f660839
yawnkinsfolk <yawnkinsfolk@gmail.com>
2021-03-08 15:13:41 +0900

    Add comment Install composer

--- init.sh
+++ init.sh
@@ -17,6 +17,7 @@ if [ `uname` = "Darwin" ]; then
   # Install nvm
   /bin/bash -c "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash)"
 
+  # Install composer
   # https://getcomposer.org/doc/00-intro.md#globally
   php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
   php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
