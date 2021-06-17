1c7fcce15ae1eb0b5f867108976fbcd491a624ab
Tomonori Takanawa <takanawa@hey.com>
2021-06-17 21:19:01 +0900

    Delete creating a symbolic link og vscode setting file

--- ln.sh
+++ ln.sh
@@ -3,4 +3,3 @@
 ln -s $HOME/dotfiles/.zshrc $HOME/
 ln -s $HOME/dotfiles/.gitconfig $HOME/
 ln -s $HOME/dotfiles/.gitignore_global $HOME/
-ln -s $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/
