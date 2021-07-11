05a0e13d2507f03a8006610419c05f56d5c3b8ca
Tomonori Takanawa <takanawa@hey.com>
2021-07-12 01:30:08 +0900

    Revert "Delete creating a symbolic link og vscode setting file"
    This reverts commit 1c7fcce15ae1eb0b5f867108976fbcd491a624ab.

--- ln.sh
+++ ln.sh
@@ -3,3 +3,4 @@
 ln -s $HOME/dotfiles/.zshrc $HOME/
 ln -s $HOME/dotfiles/.gitconfig $HOME/
 ln -s $HOME/dotfiles/.gitignore_global $HOME/
+ln -s $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/
