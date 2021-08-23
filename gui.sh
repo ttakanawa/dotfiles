23035fdc451133d5e5942e2e3daaa7b8327ab908
Tomonori Takanawa <takanawa@hey.com>
2021-08-23 09:35:56 +0900

    Install GUI apps by way of Homebrew instead of screen handling.

--- gui.sh
+++ gui.sh
@@ -1,17 +1,10 @@
 #!/bin/bash -eu
 
-open https://www.mozilla.org/en-US/firefox/new/
 open https://www.jetbrains.com/toolbox/
-open https://1password.com/
-open https://www.iterm2.com/downloads.html
-open https://hub.docker.com/editions/community/docker-ce-desktop-mac
-open https://www.deepl.com/app
 open https://code.visualstudio.com/Download
 open http://www.oracle.com/technetwork/java/javase/downloads/index.html
 open https://support.logi.com/hc/en-us/articles/360025297893-Logitech-Options
 open https://skylum.com/account/my-software
-open https://slack.com/intl/en-us/downloads/mac
-open https://evernote.com/products/skitch
 open https://discordapp.com/
 
 # Xcode
