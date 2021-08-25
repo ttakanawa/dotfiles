156fb98f616409ea9aaad387d27afe69ab516f44
Tomonori Takanawa <takanawa@hey.com>
2021-08-25 21:16:21 +0900

    Add cask apps to Brewfile

--- gui.sh
+++ gui.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -eu
 
-open https://www.jetbrains.com/toolbox/
-open https://code.visualstudio.com/Download
 open http://www.oracle.com/technetwork/java/javase/downloads/index.html
 open https://support.logi.com/hc/en-us/articles/360025297893-Logitech-Options
 open https://skylum.com/account/my-software
-open https://discordapp.com/
 
 # Xcode
 open https://itunes.apple.com/jp/app/xcode/id497799835?l=en&mt=12
