e37752892b143d51998e579a08f09e95e7a74f80
Tomonori Takanawa <takanawa@hey.com>
2021-12-27 21:59:23 +0900

    Refactoring ln.sh

--- ln.sh
+++ ln.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ln -s $HOME/dotfiles/.zshrc $HOME/
-ln -s $HOME/dotfiles/.gitconfig $HOME/
-ln -s $HOME/dotfiles/.gitignore_global $HOME/
-ln -s $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/code/user/
-ln -s $HOME/dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/code/user/
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
