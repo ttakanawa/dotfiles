3183a74849c8d3d063f89f38aca2a77a0242eac7
Tomonori Takanawa <takanawa@hey.com>
2021-12-27 22:16:00 +0900

    Add to remove before creating symbolic links on ln.sh

--- ln.sh
+++ ln.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+rm ~/.gitconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
+rm ~/.gitignore_global
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
