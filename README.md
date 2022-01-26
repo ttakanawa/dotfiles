a4636cada1c65b6a992040ec898869206d0fb889
Tomonori Takanawa <takanawa@hey.com>
2022-01-26 13:49:11 +0900

    Refactor initializing zsh

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 ```
 $ git clone git@github.com:ttakanawa/dotfiles.git $HOME/dotfiles
 $ cd $HOME/dotfiles
-$ ./ln.sh
 $ ./init.sh
 $ ./homebrew/init.sh
 $ ./git/init.sh
+$ ./zsh/init.sh
 $ ./vscode/init.sh
 $ zsh
 ```
--- init.sh
+++ init.sh
@@ -1,14 +1,8 @@
-#!/bin/bash -eu
+#!/bin/bash
 
 cd $HOME/dotfiles
 
 if [ `uname` = "Darwin" ]; then
-
-
   # If you generate ssh key
   # ssh-keygen -t rsa -b 4096 -C "takanawa@hey.com"
 fi
-
-git submodule init
-git submodule update
-zsh
