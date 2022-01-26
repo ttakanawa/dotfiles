fad1b14d22698b3f2c8850d885712c9073cb5a6f
Tomonori Takanawa <takanawa@hey.com>
2022-01-26 13:48:25 +0900

    Refactor initializing homebrew

--- init.sh
+++ init.sh
@@ -3,13 +3,7 @@
 cd $HOME/dotfiles
 
 if [ `uname` = "Darwin" ]; then
-  # Install homebrew
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  # Install homebrew all packages I use
-  brew bundle
-  rm $HOME/dotfiles/Brewfile
-  brew bundle dump
 
   # If you generate ssh key
   # ssh-keygen -t rsa -b 4096 -C "takanawa@hey.com"
