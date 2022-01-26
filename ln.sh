b460a36aa55336bc4ba0f2fb9e2d4daa33fb26c8
Tomonori Takanawa <takanawa@hey.com>
2022-01-26 13:39:22 +0900

    Refactor initializing git

--- ln.sh
+++ ln.sh
@@ -3,8 +3,3 @@
 rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 
-rm ~/.gitconfig
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-
-rm ~/.gitignore_global
-ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
