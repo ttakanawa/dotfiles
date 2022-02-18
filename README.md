f66eb319c689923d80fa49a2ee927fdd4721780a
Tomonori Takanawa <takanawa@hey.com>
2022-02-18 14:50:09 +0900

    Update init.sh
    Move initializing command from README.md to init.sh.

--- README.md
+++ README.md
@@ -6,9 +6,4 @@
 $ git clone git@github.com:ttakanawa/dotfiles.git $HOME/dotfiles
 $ cd $HOME/dotfiles
 $ ./init.sh
-$ ./homebrew/init.sh
-$ ./git/init.sh
-$ ./vscode/init.sh
-$ ./zsh/init.sh
-$ zsh
 ```
--- init.sh
+++ init.sh
@@ -6,3 +6,9 @@ if [ `uname` = "Darwin" ]; then
   # If you generate ssh key
   # ssh-keygen -t rsa -b 4096 -C "takanawa@hey.com"
 fi
+
+./homebrew/init.sh
+./git/init.sh
+./vscode/init.sh
+./zsh/init.sh
+zsh
