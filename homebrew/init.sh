26913a65b99c9a7759f3c224197ee405f9051bd3
Tomonori Takanawa <takanawa@hey.com>
2022-05-16 01:32:25 +0900

    update homebrew/init.sh

--- homebrew/init.sh
+++ homebrew/init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 cd $HOME/dotfiles
+BREWFILE=$HOME/dotfiles/homebrew/Brewfile
 
 if [ `uname` = "Darwin" ]; then
   # Install homebrew
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   # Install homebrew all packages I use
-  brew bundle
-  rm $HOME/dotfiles/homebrew/Brewfile
-  brew bundle dump --file $HOME/dotfiles/homebrew/Brewfile
+  brew bundle --file ${BREWFILE}
+  rm ${BREWFILE}
+  brew bundle dump --file ${BREWFILE}
 fi
