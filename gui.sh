271536c4f24a1d0017369786f7b4937035b98861
Tomonori Takanawa <takanawa@hey.com>
2025-03-20 17:35:03 +0800

    Adds Skitch app link to installation script because it's became disabled on Homebrew
    Introduces a new link for the Skitch app to the installation script, enhancing the resources available for users.
    
    Updates the existing links for better access to required applications.

--- gui.sh
+++ gui.sh
@@ -10,3 +10,6 @@ open https://go.dev/doc/install
 open https://itunes.apple.com/jp/app/xcode/id497799835?l=en&mt=12
 # Line
 open https://apps.apple.com/jp/app/line/id539883307?l=en&mt=12
+
+# Skitch
+open https://apps.apple.com/jp/app/skitch-snap-mark-up-share/id425955336?l=en-US&mt=12
