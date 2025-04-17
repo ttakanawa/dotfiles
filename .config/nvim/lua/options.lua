061c03dd22b66890a419072425b519f14a1b4f26
Tomonori Takanawa <takanawa@hey.com>
2025-04-18 00:21:45 +0900

    Init lua config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,14 @@
+local opt = vim.opt
+
+-- Display line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- Tab and indent settings
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+-- Search settings
+opt.ignorecase = true
+opt.smartcase = true
