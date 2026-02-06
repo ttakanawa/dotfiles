0e483f3e8d9fbf0b1a51eb0ec0a7d7f95dc6c7c2
Tomonori Takanawa <t@tknw.tw>
2026-02-06 09:18:02 +0800

    Add file path copy keybindings to Neovim
    Implement <leader>y keybindings to copy relative file paths to
    clipboard. Normal mode copies the file path, while visual mode
    includes the selected line range in GitHub format (#L1-L10).

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -22,3 +22,54 @@ map("i", "<C-h>", "<BS>", opts) -- Delete previous character
 map("i", "<C-k>", "<C-o>D", opts) -- Delete to end of line
 
 -- Copy file path
+local function copy_to_clipboard(path, description)
+  if path and path ~= "" then
+    vim.fn.setreg("+", path)
+    -- vim.fn.setreg('"', path)
+    vim.notify(description .. ": " .. path, vim.log.levels.INFO)
+  else
+    vim.notify("No file path to copy", vim.log.levels.WARN)
+  end
+end
+
+local function get_relative_path()
+  local current_file = vim.fn.expand("%:p")
+  local cwd = vim.fn.getcwd()
+
+  if current_file == "" then
+    vim.notify("No file path to copy", vim.log.levels.WARN)
+    return nil
+  end
+
+  local relative_path = vim.fn.fnamemodify(current_file, ":s?" .. cwd .. "/??")
+
+  if relative_path == current_file then
+    vim.notify("File is outside current working directory", vim.log.levels.ERROR)
+    return nil
+  end
+
+  return relative_path
+end
+
+map("n", "<leader>y", function()
+  local relative_path = get_relative_path()
+  if relative_path then
+    copy_to_clipboard(relative_path, "Relative path copied")
+  end
+end, { desc = "Copy relative path to clipboard" })
+
+map("v", "<leader>y", function()
+  local relative_path = get_relative_path()
+  if not relative_path then
+    return
+  end
+
+  local start_line = vim.fn.line("v")
+  local end_line = vim.fn.line(".")
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+
+  local result = relative_path .. "#L" .. start_line .. "-L" .. end_line
+  copy_to_clipboard(result, "Path with range copied")
+end, { desc = "Copy relative path with selection range to clipboard" })
