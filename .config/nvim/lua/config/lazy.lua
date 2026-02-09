ee2e25bc2015a4942913189eba7a87f48c6b3092
Tomonori Takanawa <t@tknw.tw>
2026-02-09 21:07:34 +0900

    Revert LazyVim to default state
    Remove PHP language extras import to restore default configuration.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -18,8 +18,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- lazyvim extras
-    { import = "lazyvim.plugins.extras.lang.php" },
     -- import/override with your plugins
     { import = "plugins" },
   },
