45220172a89ccf540b71bd30cf067fb1e40717b1
Tomonori Takanawa <t@tknw.tw>
2026-02-10 23:00:06 +0800

    Use idiomatic lazy.nvim style for zk-nvim config

--- .config/nvim/lua/plugins/zk-nvim.lua
+++ .config/nvim/lua/plugins/zk-nvim.lua
@@ -1,26 +1,19 @@
 return {
+  {
     "zk-org/zk-nvim",
-    config = function()
-      require("zk").setup({
-        -- Can be "telescope", "fzf", "fzf_lua", "minipick", "snacks_picker",
-        -- or select" (`vim.ui.select`).
-        picker = "select",
-  
-        lsp = {
-          -- `config` is passed to `vim.lsp.start(config)`
-          config = {
-            name = "zk",
-            cmd = { "zk", "lsp" },
-            filetypes = { "markdown" },
-            -- on_attach = ...
-            -- etc, see `:h vim.lsp.start()`
-          },
-  
-          -- automatically attach buffers in a zk notebook that match the given filetypes
-          auto_attach = {
-            enabled = true,
-          },
+    main = "zk",
+    opts = {
+      picker = "select",
+      lsp = {
+        config = {
+          name = "zk",
+          cmd = { "zk", "lsp" },
+          filetypes = { "markdown" },
         },
-      })
-    end,
-  }
+        auto_attach = {
+          enabled = true,
+        },
+      },
+    },
+  },
+}
