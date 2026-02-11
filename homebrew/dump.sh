3658425c280b585e7c3f4e69d7c71b3898516e57
Tomonori Takanawa <t@tknw.tw>
2026-02-11 13:12:03 +0800

    Exclude Go packages from Brewfile dump

--- homebrew/dump.sh
+++ homebrew/dump.sh
@@ -6,4 +6,4 @@ BREWFILE=$HOME/dotfiles/homebrew/Brewfile
 if [ -f ${BREWFILE} ]; then
   rm ${BREWFILE}
 fi
-brew bundle dump --no-vscode --file ${BREWFILE}
+brew bundle dump --no-vscode --no-go --file ${BREWFILE}
