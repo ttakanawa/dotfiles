588bce3c8b4501aff94607db88234f0aa8e9c128
Tomonori Takanawa <t@tknw.tw>
2026-02-20 00:32:31 +0900

    Add Alt-Up/Down keymaps for picker history navigation

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,14 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      win = {
+        input = {
+          keys = {
+            ["<M-Up>"] = { "history_back", mode = { "i", "n" } },
+            ["<M-Down>"] = { "history_forward", mode = { "i", "n" } },
+          },
+        },
+      },
       sources = {
         explorer = {
           hidden = true,
