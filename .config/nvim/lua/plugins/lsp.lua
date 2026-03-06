7da6228d6fff4ffd0a59a2ff8b3fa58133017568
Tomonori Takanawa <t@tknw.tw>
2026-03-06 13:17:31 +0900

    chore(nvim/lsp): add .git as intelephense root marker

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       servers = {
         intelephense = {
-          root_markers = { "vendor" },
+          root_markers = { "vendor", ".git" },
           init_options = {
             licenceKey = os.getenv("INTELEPHENSE_LICENSE_KEY"),
             storagePath = vim.fn.stdpath("cache") .. "/intelephense",
