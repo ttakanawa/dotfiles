b56bfd7501c6b552ce8b73cc5399c7d87dcd57e2
Tomonori Takanawa <t@tknw.tw>
2026-03-17 09:08:22 +0800

    config(nvim): exclude picker inputs from minuet auto-trigger

--- .config/nvim/lua/plugins/minuet.lua
+++ .config/nvim/lua/plugins/minuet.lua
@@ -32,6 +32,7 @@ return {
         },
         virtualtext = {
           auto_trigger_ft = { "*" },
+          auto_trigger_ignore_ft = { "TelescopePrompt", "snacks_picker_input" },
           keymap = {
             accept = "<A-A>",
             accept_line = "<A-a>",
