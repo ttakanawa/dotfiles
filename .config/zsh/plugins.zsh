221bf96196faf7836659095bf2a4b2957c201e9a
Tomonori Takanawa <t@tknw.tw>
2026-06-08 22:07:41 +0800

    feat(zsh): add AWS CLI completion
    Enable tab completion for AWS CLI commands using bashcompinit
    bridge and the aws_completer binary.

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -5,3 +5,7 @@ eval "$(zoxide init zsh)"
 eval "$(direnv hook zsh)"
 
 eval "$(~/.local/bin/mise activate zsh)"
+
+# AWS CLI completion
+autoload bashcompinit && bashcompinit
+complete -C '/usr/local/bin/aws_completer' aws
