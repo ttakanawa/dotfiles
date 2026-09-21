bfdae3e7fd8f64b7cae33bf122f7c6f8c09704bc
Tomonori Takanawa <t@tknw.tw>
2026-09-21 13:46:58 +0800

    feat(nvim): recognize xcstrings files as JSON

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.filetype.add({
+  extension = {
+    xcstrings = "json",
+  },
+})
+
 vim.opt.conceallevel = 0
 vim.opt.scrolloff = 999
 vim.g.root_spec = { "cwd" }
@@ -14,4 +20,3 @@ vim.opt.listchars = {
   trail = "·",
   nbsp = "␣",
 }
-
